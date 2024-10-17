3f4b6c78210d34d8d9639cf041f8a2aed7295419
Bruno Penteado <polaco@gmail.com>
2024-10-17 15:16:19 +0100

    numstat working when buckets is zero

--- numstat/stats.go
+++ numstat/stats.go
@@ -13,7 +13,7 @@ type Stats struct {
 	Entries     int
 	UniqEntries int
 	Buckets     []Bucket
-	Percentiles []Percentile
+	Percentiles map[float64]float64
 	Min         float64
 	Max         float64
 	Sum         *big.Float
@@ -33,11 +33,6 @@ type Range struct {
 type Bucket struct {
 	Range
 	Entries int
-}
-
-type Percentile struct {
-	Percentile float64
-	Value      float64
 }
 
 func CalcStats(data []float64, numBuckets int) Stats {
@@ -78,24 +73,36 @@ func CalcStats(data []float64, numBuckets int) Stats {
 	if numBuckets > stats.UniqEntries {
 		numBuckets = stats.UniqEntries
 	}
-	stats.Buckets = make([]Bucket, numBuckets)
-	numBucketsF := float64(numBuckets)
-	bucketRange := (stats.Max - stats.Min) / numBucketsF
-	for idx := range stats.Buckets {
-		stats.Buckets[idx].Start = stats.Min + float64(idx)*bucketRange
-		stats.Buckets[idx].End = stats.Min + float64(idx+1)*bucketRange
-	}
-	for _, datum := range data {
-		// bucketing
-		bucketIdx := int((datum - stats.Min) / bucketRange)
-		if bucketIdx == numBuckets {
-			bucketIdx = numBuckets - 1
-		}
-		stats.Buckets[bucketIdx].Entries++
 
-		// variance
-		stats.Var += math.Pow(datum-stats.Avg, 2) / lengthF
+	if numBuckets > 0 {
+		// bucketing enabled
+		stats.Buckets = make([]Bucket, numBuckets)
+		numBucketsF := float64(numBuckets)
+		bucketRange := (stats.Max - stats.Min) / numBucketsF
+		for idx := range stats.Buckets {
+			stats.Buckets[idx].Start = stats.Min + float64(idx)*bucketRange
+			stats.Buckets[idx].End = stats.Min + float64(idx+1)*bucketRange
+		}
+		for _, datum := range data {
+			// bucketing
+			if numBuckets > 0 {
+				bucketIdx := int((datum - stats.Min) / bucketRange)
+				if bucketIdx == numBuckets {
+					bucketIdx = numBuckets - 1
+				}
+				stats.Buckets[bucketIdx].Entries++
+			}
+
+			// variance
+			stats.Var += math.Pow(datum-stats.Avg, 2) / lengthF
+		}
+	} else {
+		// bucketing disabled,  calculate only variance then
+		for _, datum := range data {
+			stats.Var += math.Pow(datum-stats.Avg, 2) / lengthF
+		}
 	}
+
 	// standard deviation
 	stats.StdDev = math.Sqrt(stats.Var)
 
@@ -109,37 +116,23 @@ func CalcStats(data []float64, numBuckets int) Stats {
 	//
 	// Percentiles
 	//
-	stats.Percentiles = []Percentile{}
-	addPct := func(pct float64, value float64) {
-		entry := Percentile{
-			Percentile: pct,
-			Value:      value,
-		}
-		stats.Percentiles = append(stats.Percentiles, entry)
+	applyPercentiles := []float64{
+		// p0.0001 towards p0.1
+		0.0001, 0.001, 0.01, 0.1,
+		// p1 towards p10
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+		// p20 towards p90
+		20, 30, 40, 50, 60, 70, 80, 90,
+		// p91 towards p99
+		91, 92, 93, 94, 95, 96, 97, 98, 99,
+		// p99.9 towards p99.9999
+		99.9, 99.99, 99.999, 99.9999,
 	}
-	// linear progression (+1) from pct1 to pct10 and pct90 to pct99
-	for i := 1; i < 10; i++ {
-		lowIdx := float64(i) / 100 * (lengthF - 1)
-		highIdx := lengthF - 1 - lowIdx
-		addPct(float64(i), data[int(math.Round(lowIdx))])
-		addPct(100-float64(i), data[int(math.Round(highIdx))])
+	stats.Percentiles = map[float64]float64{}
+	for _, percentile := range applyPercentiles {
+		idx := percentile / 100 * (lengthF - 1)
+		stats.Percentiles[percentile] = data[int(math.Round(idx))]
 	}
-	// linear progression (+10) from pct10 to pct90
-	for i := 10; i <= 90; i += 10 {
-		idx := float64(i) / 100 * (lengthF - 1)
-		addPct(float64(i), data[int(math.Round(idx))])
-	}
-	// logaritmic progression from pct0.0001 to pct0.1
-	// and from pct99.9 to pct99.9999
-	for digits := 1; digits <= 4; digits++ {
-		pct1 := 1.0 / math.Pow(10, float64(digits))
-		pct99 := 100.0 - pct1
-		addPct(pct1, data[int(pct1/100.0*(lengthF-1))])
-		addPct(pct99, data[int(pct99/100.0*(lengthF-1))])
-	}
-	sort.Slice(stats.Percentiles, func(i, j int) bool {
-		return stats.Percentiles[i].Percentile < stats.Percentiles[j].Percentile
-	})
 
 	return stats
 }
@@ -158,10 +151,25 @@ func (s *Stats) Print(writer io.Writer) error {
 	fmt.Fprintf(writer, "er95.0\t%.5f ~ %.5f\n", s.ER950.Start, s.ER950.End)
 	fmt.Fprintf(writer, "er99.7\t%.5f ~ %.5f\n", s.ER997.Start, s.ER997.End)
 
-	for _, pct := range s.Percentiles {
-		fmt.Fprintf(writer, "pct%-5g\t%.5f\n", pct.Percentile, pct.Value)
+	// print percentiles in order
+	type percentile struct {
+		p float64
+		v float64
+	}
+	sortedPercentiles := make([]percentile, len(s.Percentiles))
+	idx := 0
+	for pct, value := range s.Percentiles {
+		sortedPercentiles[idx] = percentile{pct, value}
+		idx++
+	}
+	sort.Slice(sortedPercentiles, func(i, j int) bool {
+		return sortedPercentiles[i].p < sortedPercentiles[j].p
+	})
+	for _, pct := range sortedPercentiles {
+		fmt.Fprintf(writer, "pct%-5g\t%.5f\n", pct.p, pct.v)
 	}
 
+	// print buckets along with bar graphs
 	digits := int(math.Log10(float64(s.Entries))) + 1
 	fmtStr := "bkt%02d\t%.5f ~ %.5f\tentries %-" + strconv.Itoa(digits) + "d\t%0.2f%%\t%s\n"
 	for idx, bucket := range s.Buckets {
